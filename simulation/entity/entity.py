545af4d125ddff30bc8e71f8dbf2d0bd215979ee
shun <zenshun10@gmail.com>
2025-12-11 03:17:24 +0900

    12/11 Fix entity

--- simulation/entity/entity.py
+++ simulation/entity/entity.py
@@ -3,24 +3,11 @@ import abc
 
 class Entity(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def __init__(self, position=None):
-        if position is None:
-            position = [0.0, 0.0, 0.0]
-        self.position = position
+    def __init__(self, scene = None, surface=None):
+        self.scene = scene
+        self.surface = surface
 
     @abc.abstractmethod
-    def create(self, *args, **kwargs):
+    def create(self):
         pass
 
-class Robot(Entity):
-    @abc.abstractmethod
-    def __init__(self):
-        pass
-
-    @abc.abstractmethod
-    def create(self, *args, **kwargs):
-        pass
-
-    @abc.abstractmethod
-    def action(self, *args, **kwargs):
-        pass
